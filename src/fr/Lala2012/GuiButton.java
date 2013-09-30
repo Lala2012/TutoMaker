108230c665120d9241c6e97381fd470f36eb101e
victor <victor@hactir.rowanet.tbl>
2013-09-30 08:58:03 +0200

    Petit test

--- src/fr/Lala2012/GuiButton.java
+++ src/fr/Lala2012/GuiButton.java
@@ -4,6 +4,8 @@ import java.awt.Color;
 
 import javax.swing.JButton;
 
+
+/** petit commentaire pour test */
 public class GuiButton extends JButton {
 	
 	private static final long serialVersionUID = 1L;
